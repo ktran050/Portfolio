727eda888258c9b6460616c08c8142009520ab3d
ktran0501 <ktran050@ucr.edu>
2016-06-29 23:47:04 +0000

    improved input prompting and results display

--- cprogramming.com/ArrayChallenge.cpp
+++ cprogramming.com/ArrayChallenge.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 int main(){
     int array[8];
-    for(int x=0; x<8; x++)
-    cin>>array[x];
+    for(int x=0; x<8; x++){
+        cout<<"Please enter one digit"<<endl;
+        cin>>array[x];
+    }
     for(int x=0; x<8; x++)
     cout<<array[x];
+    cout<<endl;
     return 0;
 }
