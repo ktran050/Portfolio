fa6d9c50e2d2a9c03e6d2da22bc7f3c0de3628b5
ktran0501 <ktran050@ucr.edu>
2016-06-29 23:42:47 +0000

    initial commit ArrayChallenge

---
+++ cprogramming.com/ArrayChallenge.cpp
@@ -0,0 +1,13 @@
+// URL: http://www.cprogramming.com/complete/array.html
+// Date/Time: 6/29/16 4:38 PM
+#include <iostream>
+using namespace std;
+
+int main(){
+    int array[8];
+    for(int x=0; x<8; x++)
+    cin>>array[x];
+    for(int x=0; x<8; x++)
+    cout<<array[x];
+    return 0;
+}
