95b35de23964f65b40dc562ce6d99134cb33abf4
Kevin Tran <ktran050@ucr.edu>
2017-11-03 15:51:38 -0700

    priority scheduling now works

--- xv6/proc.c
+++ xv6/proc.c
@@ -401,7 +401,7 @@ int pset(int pVal){
 void
 scheduler(void)
 {
-  struct proc *p;
+  struct proc *p, *h, *i;
   struct cpu *c = mycpu();
   c->proc = 0;
   
@@ -412,17 +412,35 @@ scheduler(void)
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
+    h=ptable.proc;
+
+      // Find a baseline value
+      for(i = ptable.proc; i < &ptable.proc[NPROC]; i++){
+        if(i->state == RUNNABLE){
+          h = i;
+          i = &ptable.proc[NPROC];
+        }
+      }
+
+      // Find the highest priority value
+      for(i = ptable.proc; i < &ptable.proc[NPROC]; i++){
+        if((i->state == RUNNABLE)&&(i->pVal > h->pVal))
+          h = i;
+      }
+
+      if(p != h)
+        continue;
+      if(h->state != RUNNABLE)
         continue;
 
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
+      c->proc = h;
+      switchuvm(h);
+      h->state = RUNNING;
 
-      swtch(&(c->scheduler), p->context);
+      swtch(&(c->scheduler), h->context);
       switchkvm();
 
       // Process is done running for now.
