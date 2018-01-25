c337a7452d86af340385f40572726b823f3cca0b
Kevin Tran <ktran050@hammer.cs.ucr.edu>
2018-01-24 21:40:26 -0800

    possibly got ambient light for phong_shader

--- currentCourses/cs130/assignment1/src/phong_shader.cpp
+++ currentCourses/cs130/assignment1/src/phong_shader.cpp
@@ -8,7 +8,8 @@ vec3 Phong_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& same_side_normal,int recursion_depth,bool is_exiting) const 
 {
-    vec3 color;
-    // TODO: determine the color
-    return color;
+    vec3 ambientLight, diffuseLight, specularLight;
+    // CS130
+    ambientLight = (world.ambient_color+color_ambient)*world.ambient_intensity;
+    return ambientLight+diffuseLight+specularLight;
 }
