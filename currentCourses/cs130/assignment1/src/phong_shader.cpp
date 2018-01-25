5b54df4b472e472f8613007edf5c78cd297f5a29
Kevin Tran <ktran050@hammer.cs.ucr.edu>
2018-01-24 22:04:02 -0800

    framework for phong_shader.cpp

--- currentCourses/cs130/assignment1/src/phong_shader.cpp
+++ currentCourses/cs130/assignment1/src/phong_shader.cpp
@@ -10,6 +10,15 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 {
     vec3 ambientLight, diffuseLight, specularLight;
     // CS130
+    // ambientLight is world.ambient_color, color_ambient, world.ambient_intensity
     ambientLight = (world.ambient_color+color_ambient)*world.ambient_intensity;
+    	// diffuseLight is color_diffuse, diffuse_intensity, light color/
+    	// (distance which is light - intersection)^2
+    	// light color = Emitted_Light(any ray)
+    // specularLight
+    // specular intensity, light color/(distance which is light-intersection)^2
+    	// R=(2dot(L,N)*N-L)
+    	// specular intensity = max(dot(R,C),0)^a (a == specular_power) 
+    // shadows
     return ambientLight+diffuseLight+specularLight;
 }
