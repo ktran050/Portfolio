15dc24b3cbe9537fc260917848572a943f85f5c1
Kevin Tran <ktran050@hammer.cs.ucr.edu>
2018-01-26 11:45:56 -0800

    get test 05 to work

--- currentCourses/cs130/assignment1/src/phong_shader.cpp
+++ currentCourses/cs130/assignment1/src/phong_shader.cpp
@@ -10,38 +10,40 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& same_side_normal,int recursion_depth,bool is_exiting) const 
 {
     vec3 ambientLight, diffuseLight, specularLight;
-    vec3 N = same_side_normal;
+    vec3 N = same_side_normal.normalized();
     // CS130
     // ambientLight is world.ambient_color, color_ambient, world.ambient_intensity
     ambientLight = world.ambient_color*color_ambient*world.ambient_intensity;
 
     for(unsigned i = 0; i < world.lights.size(); i++){
-	vec3 dist = world.lights[i]->position - intersection_point;	// Ray between light and intersection 
+	vec3 dist = intersection_point-world.lights[i]->position;	// Ray between light and intersection 
 	vec3 L = world.lights[i]->Emitted_Light(ray);
-	L /= dot(dist,dist) * dot(dist,dist);				// color of light is proportional to distance^2
-
+	L = L/dist.magnitude_squared();				// color of light is proportional to distance^2
+	
+	dist = world.lights[i]->position - intersection_point; dist = dist.normalized();
 	float diffuseIntensity = 0;	// set diffuseIntensity
-	dist=dist.normalized();
-	if(dot(L,N) > 0){ diffuseIntensity = dot(L,N); }	// make sure its not negative
+	diffuseIntensity = dot(dist,N);
+	if(diffuseIntensity < 0) diffuseIntensity = 0;// make sure its not negative
 
         float specularIntensity = 0;	// set specularIntensity
-	vec3 refDir = (N*dot(L,N)*2-L).normalized();	// reflected light direction
-	specularIntensity = pow(dot(refDir, world.camera.position), specular_power);
+	vec3 ttemp = (intersection_point - ray.endpoint).normalized();
+	vec3 refDir = (dist-N*(dot(dist,N)*2)).normalized();	// reflected light direction
+	specularIntensity = pow(dot(ttemp,refDir), specular_power);
 	if(specularIntensity < 0){ specularIntensity = 0; }	// if neg make it 0
 
-	if(world.enable_shadows == false){	// if shadows disabled just immediately add values
+//	if(world.enable_shadows == false){	// if shadows disabled just immediately add values
 	    diffuseLight += L*color_diffuse*diffuseIntensity;
-	    specularLight += L*specularIntensity;
-	}/*
-	else{
-	    if(){ // only if no objects block the light
-	        diffuseLight += L*color_diffuse*diffuseIntensity;
-	        specularLight += L*specularIntensity;
-	    }
-	}*/
+	    specularLight += L*color_specular*specularIntensity;
+//	}
+//	else{
+//	    if(){ // only if no objects block the light
+//	        diffuseLight += L*color_diffuse*diffuseIntensity;
+//	        specularLight += L*specularIntensity;
+//	    }
+    }
 	
 
-    }
+    
     	// diffuseLight is color_diffuse, diffuse_intensity, light color/
     	// (distance which is light - intersection)^2
     	// magnitude of a vector  = sqrt( dot(n,n));
@@ -51,139 +53,6 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     	// R=(2dot(L,N)*N-L)
     	// specular intensity = max(dot(R,C),0)^a (a == specular_power) 
     // shadows
-    return ambientLight+diffuseLight+specularLight;
+    //return ambientLight+diffuseLight+specularLight;
+    return ambientLight + diffuseLight + specularLight;
 }
-
-/*
-#include "light.h"
-#include "phong_shader.h"
-#include "ray.h"
-#include "render_world.h"
-
-vec3 Phong_Shader::
-Shade_Surface(const Ray& ray,const vec3& intersection_point,
-    const vec3& same_side_normal,int recursion_depth) const
-{
-	vec3 color;
-    vec3 colorA;
-	colorA = colorA + color_ambient * this->world.ambient_color * this->world.ambient_intensity;
-    vec3 colorD;
-    vec3 colorS;
-    // TODO: determine the color
-    //phong shder equation
-    //I = RaLa + RdL max(0,l.n) + RsL max(0,v.r)^alapha
-    //n = same_side_normal	*
-	//l = ray from intersection to light.position --> light.position - intersection
-	//v = ray from intersection to ray.endpoint --> endpoint -intersection
-	
-	//p = intersection point	*
-	//Ra = color_ambient	*
-	//Rd = color_diffuse	*
-	//Rs = color_specular	*
-	//alpha = specular_power	*
-	//l
-    
-    //point source 	LightOriginal /	mag|p-p0| ^2 
-    for(unsigned i = 0; i < this->world.lights.size(); i++ )
-    {
-		//used to temperarily hold vectors needed
-		vec3 temp;
-		
-		//vec3 Ra = color_ambient;
-		vec3 Rd = color_diffuse;
-		vec3 Rs = color_specular;
-		
-		//vec3 La = this->world.lights[i]->color;
-		
-		//get L not divded by distance yet
-		vec3 L = this->world.lights[i]->Emitted_Light(ray);
-	
-		// find the vector light source to intersection point 
-		temp = intersection_point -  this->world.lights[i]->position;
-		//square the magnitude of the vector
-		float dis_L_Ipt = temp.magnitude_squared();
-		//divide light intensity by distance squared
-		L  = L / dis_L_Ipt;
-		
-		vec3 v = intersection_point - ray.endpoint;
-		v = v.normalized();
-		
-		vec3 n = same_side_normal;
-		n = n.normalized();
-		vec3 l = this->world.lights[i]->position - intersection_point;
-		l = l.normalized();
-		//n need to be normalized ** maybe l need to be normalized?
-		vec3 r = l.normalized() - n.normalized() * (2 * dot(l.normalized(),n.normalized()));
-		r = r.normalized();
-		//max(0,l.n)
-		float ldotn = dot(l,n);
-		
-		if(ldotn < 0)
-		{
-			ldotn = 0;
-		}
-
-		
-		//max(0,v.r)
-		float vdotr = dot(v,r);
-		if(vdotr < 0)
-		{
-			vdotr = 0;
-		}
-		
-		//check if shadow enabled
-		if(this->world.enable_shadows)
-		{
-		
-		
-			//Draw a ray from intersection point to light
-			Ray Int_to_light;
-			Int_to_light.endpoint = intersection_point;
-			Int_to_light.direction =  this->world.lights[i]->position - intersection_point;
-			float Dis_int_light = Int_to_light.direction.magnitude();
-			Int_to_light.direction = Int_to_light.direction.normalized();
-			
-			
-			//this will return the closest hit or 0
-			Hit hit;
-			hit.t = 0;
-			
-			Object* FirstObj = this->world.Closest_Intersection(Int_to_light,hit);
-			
-			//find intersection point of object
-			vec3 NewPt = Int_to_light.endpoint + Int_to_light.direction * hit.t;
-			NewPt = NewPt - Int_to_light.endpoint;
-			float NewPtDis = NewPt.magnitude();
-
-
-
-			if(FirstObj != 0 && Dis_int_light > NewPtDis  ) 
-			{
-			}
-			else
-			{
-				colorD = colorD + Rd * L * ldotn;
-				colorS = colorS + Rs * L * pow(vdotr, specular_power); 
-			}
-		
-		// so now I have a ray if there is an object between pt and light --> shadow
-			//how to check... first if no hits then good
-		//			so use an if statement and not do Diffuse and Specular if true
-		//no hit
-			
-	    }
-	    else
-	    {
-		
-			colorD = colorD + Rd * L * ldotn;
-			colorS = colorS + Rs * L * pow(vdotr, specular_power); 
-		}
-		
-		
-	}
-		color = colorD + colorA + colorS;
-    
-    
-    return color;
-}
-*/
--- currentCourses/cs130/assignment1/src/plane.cpp
+++ currentCourses/cs130/assignment1/src/plane.cpp
@@ -1,6 +1,7 @@
 #include "plane.h"
 #include "ray.h"
 #include <cfloat>
+#include <limits>
 
 
 // Intersect with the half space defined by the plane.  The plane's normal
@@ -12,26 +13,44 @@ Intersection(const Ray& ray, std::vector<Hit>& hits) const
     // CS130
     Hit hit;
     hit.object=this;
-    hit.ray_exiting = true;
+    float x1raynormaldot = dot(ray.endpoint-x1, normal);
 
-    float nraydot = dot(normal, ray.direction);
-    float tmpT = dot(normal, x1-ray.endpoint)/nraydot;
+    float angle = dot(ray.direction, normal);	// angle between the ray and the normal of the plane
+    if( x1raynormaldot <= 0){
+      if(angle > 0){	// ray leaves
+	  hit.t = 0; hit.ray_exiting = false;
+	  hits.push_back(hit);
 
-    if(nraydot == 0){
-	hit.t=0;
-	hits.push_back(hit);
-	return true;
+	  hit.ray_exiting = true;
+	  hit.t = -1* x1raynormaldot / angle ;
+	  hits.push_back(hit);
+	  return true;
+      }
+      else{	// ray never leaves or is inside the plane
+	  hit.t = 0; hit.ray_exiting = false;
+	  hits.push_back(hit);
+
+	  hit.t = std::numeric_limits<double>::max();
+	  hits.push_back(hit);
+	  return true;
+      }
+
     }
     else{
-	if(tmpT > 0){
-	    hit.t = tmpT;
-	    hits.push_back(hit);
-	    return true;
-	}
-	else{ return false;}
-    }
+	if(angle < 0){	
+	  hit.t = -1* x1raynormaldot / angle ;
+	  hit.ray_exiting = false;
+	  hits.push_back(hit);
 
-    return false;
+	  hit.ray_exiting = false;
+	  hit.t = std::numeric_limits<double>::max();
+	  hits.push_back(hit);
+	  return true;
+	}
+	else{
+	  return false;
+	}
+    }
 }
 
 vec3 Plane::
@@ -40,118 +59,3 @@ Normal(const vec3& point) const
     return normal;
 }
 
-/*
-#include "plane.h"
-#include "ray.h"
-#include <cfloat>
-
-// Intersect with the half space defined by the plane.  The plane's normal
-// points outside.  If the ray starts on the "inside" side of the plane, be sure
-// to record a hit with t=0 as the first entry in hits.
-bool Plane::
-Intersection(const Ray& ray, std::vector<Hit>& hits) const
-{
-
-    double raydotn = dot(ray.direction, normal);
-    Hit temp;
-    if(dot((ray.endpoint - x1), normal) <= 0)
-    {
-		if(raydotn < 0)
-		{
-//std::cerr << "RAY Surface TO INSIDE \n";
-		   //ray starts on surface of half plane and never leaves 
-		   //first hit point
-		   temp.object = this;
-		   temp.t = 0;
-		   temp.ray_exiting = false;
-		   hits.push_back(temp);
-		   
-		   //second hit point
-		   temp.object = this;
-		   temp.t = DBL_MAX;
-		   temp.ray_exiting = false;
-		   hits.push_back(temp);
-		   
-		   return true;
-		}
-		else if(raydotn > 0)
-		{
-//std::cerr << "RAY Inside to OUTSIDE  \n";
-		   //ray starts inside half plane but leaves
-		   //first hit point
-		   temp.object = this;
-		   temp.t = 0;
-		   temp.ray_exiting = false;
-		   hits.push_back(temp);
-		   
-		   //second hit point
-		   temp.object = this;
-		   temp.t = - dot((ray.endpoint - x1), normal) / raydotn ;
-		   temp.ray_exiting = true;
-		   hits.push_back(temp);
-		   
-		   return true;
-			
-		}
-		else
-		{
-		   //ray has no intersections with surface of plane, 
-		   // ray starts inside half space and never leaves 
-		   //first hit point
-//std::cerr << "RAY inside TO INSIDE \n";
-		   temp.object = this;
-		   temp.t = 0;
-		   temp.ray_exiting = false;
-		   hits.push_back(temp);
-		   
-		   //second hit point
-		   temp.object = this;
-		   temp.t = DBL_MAX;
-		   temp.ray_exiting = false;
-		   hits.push_back(temp);
-		   
-		   return true;
-		}	
-		
-	}
-	else
-	{
-		if(raydotn < 0)
-		{
-//std::cerr << "RAY OUTSIDE TO INSIDE \n";
-		   //one intersection, ray starts outside plane 
-		   //first hit point
-		   temp.object = this;
-		   temp.t = - dot((ray.endpoint - x1), normal) / raydotn ;
-		   temp.ray_exiting = false;
-		   hits.push_back(temp);
-		   
-		   //second hit point
-		   temp.object = this;
-		   temp.t = DBL_MAX;
-		   temp.ray_exiting = false;
-		   hits.push_back(temp);
-		   
-		   return true;
-		}
-		else if(raydotn > 0)
-		{
-			//doesnt intersect with plane
-			return false;
-		}
-		else
-		{
-			//doesnt intersect with plane
-			return false;
-		}	
-	}
-    
-    return false;
-}
-
-vec3 Plane::
-Normal(const vec3& point, int part) const
-{
-    return normal;
-}
-*/
