0011367a207881fdf97cdfa013f16785bb5189be
Kevin Tran <ktran050@wch132-24.cs.ucr.edu>
2018-02-28 13:09:07 -0800

    added lab8 cs130 files

---
+++ currentCourses/cs130/lab-08-files/gl_viewer.d
@@ -0,0 +1,2 @@
+gl_viewer.d gl_viewer.o: gl_viewer.cpp gl_viewer.h gl_arcball.h vec.h degRadLib.h \
+ mat.h
---
+++ currentCourses/cs130/lab-08-files/quaternion.d
@@ -0,0 +1,1 @@
+quaternion.d quaternion.o: quaternion.cpp quaternion.h vec.h degRadLib.h mat.h
